133ae9241952e680e9d41685325d7958d383ebdc
janinejad <m.tarjomeh@gmail.com>
2024-04-26 12:38:09 +0330

    colplete project

--- blog1/urls.py
+++ blog1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import hello,contact,about
+from .views import contact, about,get_plan
+
 app_name = 'blog'
 urlpatterns = [
-    path('contact-us/',contact,name='contact'),
-    path('about/',about,name='about')
+    path('contact-us/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('body-building-plan/<int:day_id>', get_plan, name='body_building_plan')
 ]
--- blog1/views.py
+++ blog1/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 # from django.shortcuts import HttpResponse
 from django.http import HttpResponse
+
+
 def hello(request):
-    return render(request,'home.html',{})
+    return render(request, 'home.html', {})
+
 
 def contact(request):
     return render(request, 'contact.html', {})
@@ -10,3 +13,24 @@ def contact(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def get_plan(request, day_id=None):
+    plans = ['اسکات پا 4*10 - لانگ 3*12', 'استراخت', 'پرس سینه 4*12 - جلوبازو 4*12', 'زیربغل سیم کش 4*12 ', 'استراحت',
+            'سرشانه آرنولدی 3*12- نشر خم 3*12', 'استراحت']
+    days = ['شنبه','یکشنبه','دوشنبه','سشنبه','چهارشنبه','پنج شنبه','جمعه']
+    try:
+        plan = ''
+        day = ''
+        plan = plans[day_id]
+        day = days[day_id]
+    except:
+        plan = 'روز مورد نظر یافت نشد'
+        day = 'روز مورد نظر یافت نشد'
+
+    context = {
+        'plan' : plan,
+        'day': day
+    }
+
+    return render(request,'bodibuilding-plan.html',context)
